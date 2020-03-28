f29479beb90e3d5552fd35977b3d0585ccc42283
Lyric_ <lirui185@126.com>
2020-03-28 15:36:54 +0800

    创建eureka注册中心和客户端

---
+++ lyric-service-test/src/test/java/com/lyric/lyricservicetest/LyricServiceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyric.lyricservicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricServiceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
