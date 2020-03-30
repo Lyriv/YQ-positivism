cfa191e9221fd308a7290cb0dd66f9d0eb91134a
Lyric_ <lirui185@126.com>
2020-03-30 20:33:19 +0800

    消费者

---
+++ lyric-web-demo/src/main/java/com/lyric/webdemo/LyricWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lyric.webdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricWebDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyricWebDemoApplication.class, args);
+    }
+
+}
---
+++ lyric-web-demo/src/test/java/com/lyric/webdemo/LyricWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyric.webdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricWebDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
